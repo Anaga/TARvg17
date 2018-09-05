645918a56f49422f74913590a6c4018ed64fd3f1
Aleksandr Bobyrev <bobyrev.bob@gmail.com>
2018-09-05 16:52:36 +0300

    Update main.cpp

--- TARvg/main.cpp
+++ TARvg/main.cpp
@@ -5,6 +5,7 @@ int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
     qDebug() << "Hello TAR vg 17!" ; // Qt debug output to console
+    qDebug() << "Let's pull it!";
 
     return a.exec();
 }
