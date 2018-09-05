19bfed0f646a80a1ef7e2d282fa38bdf150b5b02
Aleksandr Bobõrev <aleksandr.bobyrev@tthk.ee>
2018-09-05 17:49:51 +0300

    add some logic for other cases

--- TARvg/main.cpp
+++ TARvg/main.cpp
@@ -12,17 +12,52 @@ int main(int argc, char *argv[])
     qDebug() << "Let's pull it!";
     int a, b;
     int n, m, l;
-    qDebug() << "Please enter size of brick \n";
+    qDebug() << "Please enter size of brick";
     cin >> n >> m >> l;
-    QString qsSizes = "size of brick is %1x%2x%3\n";
+    QString qsSizes = "size of brick is %1x%2x%3";
     qsSizes = qsSizes.arg(n).arg(m).arg(l);
     qDebug() << qsSizes;
 
-    qDebug() << "Please enter size of hole \n";
+    qDebug() << "Please enter size of hole";
     cin >> a >> b;
-    qsSizes = "size of hole is %1x%2\n" ;
+    qsSizes = "size of hole is %1x%2" ;
     qsSizes = qsSizes.arg(a).arg(b);
     qDebug() << qsSizes;
 
+    // First check brick ( put on M side ): 20x3x4  hole: 3x5
+    {
+        if ((l<a) && (n<b)) qDebug() << "fits 1.1";
+        if ((l<b) && (n<a)) qDebug() << "fits 1.2";
+    }
+
+    // Second check ( put on N side ),
+    {
+        if ((m<a) && (l<b)) qDebug() << "fits 2.1";
+        if ((m<b) && (l<a)) qDebug() << "fits 2.2";
+    }
+
+    // Last check ( put on L side )
+    {
+        if ((m<a) && (n<b)) qDebug() << "fits 2.1";
+        if ((m<b) && (n<a)) qDebug() << "fits 2.2";
+    }
+
     return ann.exec();
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
