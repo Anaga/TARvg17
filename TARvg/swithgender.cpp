838e27ae8132970ae53c98201f2a6db4ac3b3c55
Aleksandr Bobõrev <aleksandr.bobyrev@tthk.ee>
2018-09-26 16:42:24 +0300

    some code

--- TARvg/swithgender.cpp
+++ TARvg/swithgender.cpp
@@ -1,19 +1,25 @@
 
+#include <QCoreApplication>
+#include <QDebug> //F1 for help
+#include <QTextStream>
+#include <QString>
 
 
 bool mainFunc(){
 
     QTextStream cin(stdin);
 
-    qDebug() << "Hello from swith";
+    qDebug() << "Please enter sex Ff/Mm";
     char sex;
     cin >> sex;
     switch (sex) {
     case 'M':
+    case 'm':
         qDebug() << "Male";
         return true;
         break;
     case 'W':
+    case 'w':
         qDebug() << "Female";
         return true;
         break;
