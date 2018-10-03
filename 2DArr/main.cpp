f848ee51187c78215b50b96fec72941f01fc1bd2
Aleksandr Bobõrev <aleksandr.bobyrev@tthk.ee>
2018-10-03 17:12:03 +0300

    add one function to print array context

--- 2DArr/main.cpp
+++ 2DArr/main.cpp
@@ -1,10 +1,33 @@
 #include <QCoreApplication>
 #include <QDebug>
+#include <iostream>
+using namespace std;
 
-int main(int argc, char *argv[])
+void printOneDemArr(int A[], int A_Len);
+int main()
 {
-    qDebug() << "Helo Qt from new class!";
-    QCoreApplication a(argc, argv);
+#define A_SIZE 25
+#define B_SIZE 15
+    int OneDArray[A_SIZE];
+    for (int i=0;i<A_SIZE;i++){
+        OneDArray[i] = (i+1)*3;
+    }
+    printOneDemArr(OneDArray, A_SIZE);
 
-    return a.exec();
+    int OneDArrayB[B_SIZE];
+    for (int i=0;i<B_SIZE;i++){
+        OneDArrayB[i] = (i-127)*20;
+    }
+    printOneDemArr(OneDArrayB, B_SIZE);
+    return 0;
+}
+
+void printOneDemArr(int A[], int A_Len)
+{
+    for (int i=0;i<A_Len-1;i++){
+        cout << A[i] << ", ";
+    }
+    cout << A[A_Len-1] << " ; ";
+    //cout << "\b\b" << ";" ;
+    cout << endl;
 }
